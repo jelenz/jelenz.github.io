241306070123f2f0a19bc3df0d69523e55cd24f6
James Lenz <jelenz@comcast.net>
2026-06-04 16:14:32 -0400

    After build

---
+++ docs/LocalWeather/.next/server/chunks/ssr/app_layout_tsx_2144vk_._.js
@@ -0,0 +1,3 @@
+module.exports=[33290,a=>{"use strict";var b=a.i(7997);a.s(["default",0,function({children:a}){return(0,b.jsx)("html",{lang:"en",className:"h-full antialiased",children:(0,b.jsx)("body",{children:a})})},"metadata",0,{title:"Local Weather",description:"A simple weather app"}])},70864,a=>{a.n(a.i(33290))}];
+
+//# sourceMappingURL=app_layout_tsx_2144vk_._.js.map
---
+++ docs/LocalWeather/.next/server/middleware-build-manifest.js
@@ -0,0 +1,21 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/0cz1d0mv5g_q7.js"
+  ],
+  "lowPriorityFiles": [
+    "static/C9KbnQKWsx500UltvGPN9/_buildManifest.js",
+    "static/C9KbnQKWsx500UltvGPN9/_ssgManifest.js",
+    "static/C9KbnQKWsx500UltvGPN9/_clientMiddlewareManifest.js"
+  ],
+  "rootMainFiles": [
+    "static/chunks/1jq4o6yq14o4c.js",
+    "static/chunks/0245a4ypel7xx.js",
+    "static/chunks/3n7dm2ojtyzwn.js",
+    "static/chunks/0dw6y-0ffla71.js",
+    "static/chunks/turbopack-2ytl2-0ub-f7j.js"
+  ]
+};
